b5a1f0b46b4fa913a9c6994c9c082ab16dfdc775
harpriya19 <90307264+harpriya19@users.noreply.github.com>
2021-09-08 12:27:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first_name="harpriya";
+var second_name="saigal";
+console.log(first_name);
+console.log(second_name);
+first_name.concat(second_name);
+var a="10";
+var b="5";
+var add=a+b;
+console.log(add);
+
